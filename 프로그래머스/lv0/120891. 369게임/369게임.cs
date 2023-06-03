a8e2f78991ff32ea8afccb62b4bd468db6980c29
Dabin <dorothy9795@gmail.com>
2023-06-03 23:03:06 +0900

    [level 0] Title: 369게임, Time: 1.16 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Linq;
+
+public class Solution 
+{
+    public int solution(int order)
+    {
+        var toNumber = (char)0 - '0';
+
+        var count = order
+            .ToString()
+            .ToCharArray()
+            .Count(x =>
+            {
+                var number = (x + toNumber);
+                return number != 0 && number % 3 == 0;
+            });
+
+        return count;
+    }
+}
