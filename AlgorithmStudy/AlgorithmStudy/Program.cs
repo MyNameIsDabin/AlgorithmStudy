49685645736414949fef31a1ecbd5ad2cd5b74f1
Dabin <dorothy9795@gmail.com>
2023-06-26 22:06:21 +0900

    대충 DFS 연습

--- AlgorithmStudy/AlgorithmStudy/Program.cs
+++ AlgorithmStudy/AlgorithmStudy/Program.cs
@@ -1,13 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Text;
 
 namespace AlgorithmStudy
 {
+    public class Solution 
+    {
+        public void Permutation(int[] arr, int depth)
+        {
+            if (depth == arr.Length)
+            {
+                Console.WriteLine(string.Join(", ", arr.Select(x => x.ToString()).ToArray()));
+                return;
+            }
+
+            for (var i = depth; i < arr.Length; i++)
+            {
+                Swap(ref arr[depth], ref arr[i]);
+                Permutation(arr, depth + 1);
+                Swap(ref arr[depth], ref arr[i]);
+            }
+        }
+
+        public void Swap(ref int lv, ref int rv)
+        {
+            var temp = lv;
+            lv = rv;
+            rv = temp;
+        }
+    }
+    
     internal class Program
     {
         public static void Main(string[] args)
         {
-            
+            var solution = new Solution();
+
+            solution.Permutation(new [] { 1, 2, 3 }, 0);
         }
     }
 }
---
+++ AlgorithmStudy/AlgorithmStudy/Programmers/Basic/PermutationDFS.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgorithmStudy.Programmers.Basic
+{
+    public class PermutationDFS
+    {
+        public void Permute(int[] arr, bool[] visited, int[] output, int depth)
+        {
+            if (depth == arr.Length)
+            {
+                Console.WriteLine(string.Join(",", output.Select(x => x.ToString()).ToArray()));
+                return;
+            }
+            
+            for (var i = 0; i < arr.Length; i++)
+            {
+                if (visited[i])
+                    continue;
+
+                visited[depth] = true;
+                output[depth] = arr[i];
+                Permute(arr, visited, output, depth + 1);
+                visited[depth] = false;
+            }
+        }
+        
+        public static void RunTest()
+        {
+            var solution = new PermutationDFS();
+
+            var visited = default(bool[]);
+            
+            solution.Permute(new [] { 1, 2, 3 }, visited, new int[3], 0);
+        }
+    }
+}
